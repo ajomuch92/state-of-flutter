ad3ce47d85c65e527d876bb1e5958895a01c9a6a
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2026-03-22 20:15:34 -0600

    chore: update dependencies and add JSX support in TypeScript config
    - Added @astrojs/vue and vue to dependencies in package.json
    - Added JSX preservation option in tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [vue()]
 });
